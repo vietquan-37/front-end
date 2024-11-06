773bea4697c66f3cdf7902e8cf78f2b78e750f8a
chechminh19 <meruem0812@gmail.com>
2024-11-06 21:35:42 +0700

    add logout-admin
    add

--- src/app/admin/navAdmin.tsx
+++ src/app/admin/navAdmin.tsx
@@ -2,21 +2,21 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation"; // Import usePathname
-
+import { signOut } from "next-auth/react";
 export default function Nav() {
     const pathname = usePathname(); // Get the current pathname
     const inactive = 'flex gap-1 p-7';
     const active = inactive + ' bg-white text-blue-900 rounded-l-lg';
     return (
         <aside className="text-white p-4 pr-0">
-            <Link href={'/'} className="flex gap-1 mb-4 mr-4">
+            {/* <Link href={'/'} className="flex gap-1 mb-4 mr-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
              <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
                 </svg>
         <span className="">
                  Store
             </span>
-            </Link>
+            </Link> */}
 
 <nav className="flex flex-col gap-2">
 
@@ -35,7 +35,14 @@ export default function Nav() {
     <Link href={'/admin/users'} className={pathname?.includes('/users') ? active : inactive}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
         <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
         </svg>User</Link>
-        
+        <button
+    className="mt-2 p-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition-colors"
+    onClick={() => signOut({ callbackUrl: "/login" })}
+>
+    Logout
+</button>
+
+
 </nav>
         </aside>
     )
